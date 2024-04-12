55c9f672b6ab81f49f2dcd7f7832491b85f0521c
moizali125 <ha9913083@gmail.com>
2024-04-12 22:14:30 +0500

    Arrays

---
+++ app.js
@@ -0,0 +1,148 @@
+document.write("<br>");
+document.write("<h3>Question no 01</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques1() ">Click Me</button>');
+document.write("<br>");
+document.write("<br>");
+function ques1() {
+
+}
+
+// question no 2
+
+document.write("<h3>Question no 02</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques2();">Click Me</button>');
+function ques2() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 3
+
+document.write("<h3>Question no 03</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques3()">Click Me</button>');
+function ques3() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 4
+
+document.write("<h3>Question no 04</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques4()">Click Me</button>');
+function ques4() {
+
+};
+document.write("<br>");
+document.write("<br>");
+//question no 5
+document.write("<h3>Question no 05</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques5()">Click Me</button>');
+function ques5() {
+
+}
+
+// document.write('<div id="pere"></div>')
+document.write("<br>");
+document.write("<br>");
+
+//question no 6
+
+document.write("<h3>Question no 06</h3>");
+document.write("<br>");
+document.write("<h3>Generator MarkSheet</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques6()"">Click Me</button>');
+function ques6() {
+
+}
+// document.write('<div id="result"></div>')
+document.write("<br>");
+document.write("<br>");
+
+
+//question no 7
+
+document.write("<h3>Question no 07</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques7()">Click Me</button>');
+function ques7() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+
+//question no 8
+
+document.write("<h3>Question no 08</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques8()">Click Me</button>');
+function ques8() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 9
+
+document.write("<h3>Question no 09</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques9()">Click Me</button>');
+function ques9() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 10
+
+document.write("<h3>Question no 10</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques10()">Click Me</button>');
+function ques10() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 11
+
+document.write("<h3>Question no 11</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques11()">Click Me</button>');
+function ques11() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+document.write('<div style="color: blue;background-color: black;"><br><h1>Best OF luck<span style="font-size:50px;">👋</span></h1><br></div>')//&#128075
